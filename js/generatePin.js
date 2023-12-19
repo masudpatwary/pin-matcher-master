ce3d14c22959d64dfb3a691d5698cba9d4dfb801
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-12-20 00:52:37 +0600

    update

--- js/generatePin.js
+++ js/generatePin.js
@@ -1,13 +1,12 @@
-function pinGenarate() {
+function generatePin() {
   const random = Math.round(Math.random() * 10000);
   return random;
 }
-
 const pinField = document.getElementById("pin-field");
 const inputDisplay = document.getElementById("input-display");
 
 function getPin() {
-  const pin = pinGenarate();
+  const pin = generatePin();
   const pinString = pin + "";
   if (pinString.length === 4) {
     return pin;
@@ -21,33 +20,31 @@ document.getElementById("generate-pin").addEventListener("click", () => {
   pinField.value = pin;
 });
 
-//**calculatore//
+//**//**Calculatore//**// *//
 
 document
   .getElementById("claculatore-main")
   .addEventListener("click", (event) => {
-    const btnNumber = event.target.innerText;
+    const numbers = event.target.innerText;
 
-    if (btnNumber === "C") {
+    if (numbers === "C") {
       inputDisplay.value = "";
-    } else if (btnNumber === "<") {
-      const btnNumberString = inputDisplay.value.split("");
-      btnNumberString.pop();
-      const btnNumber = btnNumberString.join("");
-
-      inputDisplay.value = btnNumber;
-      console.log(btnNumber);
-    } else if (btnNumber.length === 1) {
-      inputDisplay.value = inputDisplay.value + btnNumber;
+    } else if (numbers === "<") {
+      const inputDisplaystring = inputDisplay.value.split("");
+      inputDisplaystring.pop();
+      const numbers = inputDisplaystring.join("");
+      inputDisplay.value = numbers;
+    } else if (numbers.length === 1) {
+      inputDisplay.value = inputDisplay.value + numbers;
     }
   });
 
 document.getElementById("submit-btn").addEventListener("click", () => {
-  if (inputDisplay.value === pinField.value) {
+  if (pinField.value === inputDisplay.value) {
     document.getElementById("notify-show-match").style.display = "block";
     document.getElementById("notify-show-no-match").style.display = "none";
   } else {
-    document.getElementById("notify-show-no-match").style.display = "block";
     document.getElementById("notify-show-match").style.display = "none";
+    document.getElementById("notify-show-no-match").style.display = "block";
   }
 });
